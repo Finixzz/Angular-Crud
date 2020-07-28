5c98541dab8afa781bc4f908b03efbae830e1e28
Ahmet Durmic <ahmetdurmic97@outlook.com>
2020-07-28 15:14:04 +0200

    Setted up Create Employee html template layout

--- web-app/src/app/app.module.ts
+++ web-app/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule , Routes } from '@angular/router';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees/list-employees.component';
 import { CreateEmployeeComponent } from './employees/create-employee/create-employee.component';
+
 
 const appRoutes: Routes=[
     {path: 'list',component:ListEmployeesComponent},
@@ -20,6 +22,7 @@ const appRoutes: Routes=[
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
